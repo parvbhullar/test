b2f5d1617be2ff8c8dc092435d1cdbedcbcb1a54
parvbhullar <parvinder_singh1@yahoo.co.in>
2015-09-02 16:56:52 +0530

    no message

--- rename-files.php
+++ rename-files.php
@@ -11,14 +11,28 @@ $file = __DIR__."/data/barcode-pid.txt";
 $content = file_get_contents($file);
 
 $arr = explode("\n", $content);
-
+$nCount = 0;
 foreach($arr as $line){
     $lArr = explode("\t", $line);
     $barCode = trim($lArr[0]);
     $pId = trim($lArr[1]);
-
     //get the file and rename
     //
+    //
     //http://finisfun.com/mavericks/
-    rename("/tmp/tmp_file.txt", "/home/user/login/docs/my_file.txt");
+
+    $basePath = '/home/gce3/nc/OrderImage/';
+    $filename = $basePath.$barCode.".png";
+    if (file_exists($filename)) {
+        echo "The file $filename exists\n";
+        $nFileName = $basePath.$pId.".png";
+        echo "New file name is ". $nFileName;
+        rename($filename, $nFileName);
+
+    } else {
+        $nCount++;
+        echo "The file $filename does not exist\n";
+    }
 }
+
+echo "Total not found files $nCount\n";
